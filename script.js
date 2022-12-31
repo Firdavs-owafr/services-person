ce704606b32beca7cbc19b5fd5c3d71bce9d024e
Firdavs <firdavseminovhpl@gmail.com>
2022-12-31 12:46:57 +0500

    services

---
+++ script.js
@@ -0,0 +1,22 @@
+// fetch('//jsonplaceholder.typicode.com/users')
+    // .then(per => per.json())
+    // .then(data => console.log(data))
+
+    // function pow (x , n) {
+//     let a = 1;
+//     for(let i = 0; i < n; i++){
+//         a*= x
+//     }
+//     return a
+
+// }
+
+// console.log(pow(8 , 3));
+
+// function powRecurs(x , n){
+//     if(n === 1) {
+//         return x
+//     }else {
+//         return x * powRecurs(x , n - 1)
+//     }
+// }
